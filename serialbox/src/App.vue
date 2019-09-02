1db10f009b6f7b8d054476019400be618d0a1a9f
yanglei15 <yanglei15@meituan.com>
2019-09-02 21:29:56 +0800

    [change]构建初步的前端

---
+++ serialbox/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <mt-header title="long long long long title">
+      <router-link to="/" slot="left">
+        <mt-button icon="back">back</mt-button>
+      </router-link>
+      <mt-button icon="more" slot="right"></mt-button>
+    </mt-header>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+body {
+  padding: 0;
+  margin: 0;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
